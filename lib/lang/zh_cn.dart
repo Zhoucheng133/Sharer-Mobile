d63fdc844bda491812ec3ffd2c6f9960aa15f8d9
zhoucheng <zhoucheng133@outlook.com>
2026-06-22 17:55:31 +0800

    添加基础框架

---
+++ lib/lang/zh_cn.dart
@@ -0,0 +1,4 @@
+const Map<String, String> zhCN = {
+  'home': '主页',
+  'settings': '设置'
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:local_sink/lang/zh_cn.dart';
+import 'package:local_sink/main_view.dart';
+import 'package:local_sink/utils/controller.dart';
 
-void main() {
+Future<void> main() async {
+  final controller=Get.put(Controller());
+  await controller.init();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'zh_CN': zhCN,
+  };
+}
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  final Controller controller=Get.find();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+
+    return Obx(
+      () => GetMaterialApp(
+        translations: MainTranslations(),
+        debugShowCheckedModeBanner: false,
+        locale: controller.lang.value.locale, 
+        supportedLocales: supportedLocales.map((item)=>item.locale).toList(),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        theme: ThemeData(
+          brightness: brightness,
+          fontFamily: 'PuHui', 
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.lightBlue,
+            brightness: brightness,
+          ),
+          textTheme: brightness==Brightness.dark ? ThemeData.dark().textTheme.apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ) : ThemeData.light().textTheme.apply(),
         ),
+        home: MainView()
       ),
     );
   }
